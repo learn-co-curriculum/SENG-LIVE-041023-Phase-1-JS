08cd78278e6e62173a7e152e88ed01b98d074420
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-04-10 21:18:51 -0400

    defer

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -1,25 +1,14 @@
-//BookStore has been moved to data.js 
-console.log(bookStore);
+//console.log(bookStore)
 
-console.log(document.querySelector('div'))
+// 1. select dom
+// 2. add content to the dom
 
-function formatPrice(price) {
-  return '$' + Number.parseFloat(price).toFixed(2);
-}
 
-// create a function called renderBook(book)
-// it will take a book object as an argument
-// and create the html structure for rendering 
-// that book and insert it into our webpage!
 
-// function renderBook(book) {
-// should create an li element that looks something like this:
-  // <li class="list-li">
-  //   <h3>Eloquent JavaScript : A Modern Introduction to Programming</h3>
-  //   <p>Marjin Haverbeke</p>
-  //   <p>$10.00</p>
-  //   <img src="https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="Eloquent JavaScript cover"/>
-  //   <button>Delete</button>
-  // </li>
+// add Function to generate header 
 
-  
+// add Function to generate footer
+
+// add Function to generate header 
+
+// add Function to generate book cards
