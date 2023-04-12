3f7925e4e382cee17a20e33232d40c19a7c0d05e
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-04-12 09:01:38 -0400

    day2 afternoon lecture

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -36,10 +36,15 @@
 
     function renderFooter(){
        const footerDivs = document.querySelectorAll('footer div')
+       const bookStoreValues = [bookStore.name, bookStore.location, bookStore.number]
 
-       footerDivs[0].textContent = bookStore.name
-       footerDivs[1].textContent = bookStore.location
-       footerDivs[2].textContent = bookStore.number
+      //  footerDivs[0].textContent = bookStore.name
+      //  footerDivs[1].textContent = bookStore.location
+      //  footerDivs[2].textContent = bookStore.number
+
+       footerDivs.forEach((footerDiv, index, array) => {
+        footerDivs[index].textContent = bookStoreValues[index]
+       })
     }
 
 
@@ -49,8 +54,64 @@
     //price
     //imageUrl
 
+// bookStore.inventory.forEach(book => {
+//   //1. Create DOM elements
+//       //1 li element
+//       //1 h3 element, 2 p element, 1 img element, 1 btn element
 
+//       const parentList = document.querySelector('#book-list')
+      
+//       const bookCard = document.createElement('li')
+
+//       const title = document.createElement('h3')
+//       const author = document.createElement('p')
+//       const price = document.createElement('p')
+//       const img = document.createElement('img')
+//       const button = document.createElement('button')
+
+//   //2. Populate the DOM elements with appropriate content
+//   title.textContent = book.title
+//   author.textContent = book.author
+//   price.textContent = book.price
+//   img.src = book.imageUrl
+//   button.textContent = 'Delete' //this btn will be the same
+
+//   bookCard.className = "list-li" //matching with the CSS
+
+//   //3. Wrap / Append these to One another
+//   bookCard.append(title, author, price, img, button)
+//   parentList.append(bookCard)
+// })
+
+//inner / callback function
+function renderCard(book){
+      const parentList = document.querySelector('#book-list')     
+      const bookCard = document.createElement('li')
+
+      const title = document.createElement('h3')
+      const author = document.createElement('p')
+      const price = document.createElement('p')
+      const img = document.createElement('img')
+      const button = document.createElement('button')
+
+    //2. Populate the DOM elements with appropriate content
+    title.textContent = book.title
+    author.textContent = book.author
+    price.textContent = book.price
+    img.src = book.imageUrl
+    button.textContent = 'Delete' //this btn will be the same
+
+    bookCard.className = "list-li" //matching with the CSS
+
+    //3. Wrap / Append these to One another
+    bookCard.append(title, author, price, img, button)
+    parentList.append(bookCard)
+}
 
 
 renderHeader()
 renderFooter()
+//bookStore.inventory.forEach(renderCard) //renderCard is a callback function
+
+//bookStore.inventory.forEach(renderCard)
+bookStore.inventory.forEach(book => renderCard(book))
