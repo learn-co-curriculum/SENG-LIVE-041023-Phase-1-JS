3b6ee86872b9539baca22e2e61f14da437aa1a1d
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-04-13 11:32:43 -0400

    day4-morning

---
+++ 04_Communicating_with_the_Server/src/index.js
@@ -0,0 +1,82 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // const p = Promise.reject("CONTENT")
+
+    // p.then(value => {
+    //     console.log(`YESSSSS SUCCESS!!! ${value}`)
+    // }).catch(err => {
+    //     console.error(`NO ERROR!!! :-( ${err})`)
+    // })
+
+    //3 statuses: pending, fulfilled, rejected
+    //content : what is bing returned/ passed on once the promise is resolved
+
+    fetch("http://localhost:3000/stores") //endpoint
+        .then(resp => resp.json()) //parsing it from JSON to JS 
+        .then((data) => console.log(data))
+        .catch(err=> console.error(err))
+
+
+        //JSON : JS object notation 
+        //JSON is not vanilla JS object
+
+// Render Functions
+    // Renders Header
+    function renderHeader(store){
+        document.querySelector('h1').textContent = store.name
+    }
+    // Renders Footer
+    function renderFooter(store){
+        const footerDivs = document.querySelectorAll('footer div')
+        footerDivs[0].textContent = store.name
+        footerDivs[1].textContent = store.address
+        footerDivs[2].textContent = store.hours
+    }
+
+    function renderBookCard(cardData) {
+        const li = document.createElement('li')
+        const h3 = document.createElement('h3')
+        const pAuthor = document.createElement('p')
+        const pPrice = document.createElement('p')
+        const img = document.createElement('img')
+        const btn = document.createElement('button')
+
+        h3.textContent = cardData.title
+        pAuthor.textContent = cardData.author
+        pPrice.textContent = `$${cardData.price}`
+        btn.textContent = 'Delete'
+
+        img.src = cardData.imageUrl
+        li.className = 'list-li'
+
+        //Event Listeners 
+        btn.addEventListener('click',()=>li.remove())
+    
+        li.append(h3,pAuthor,pPrice,img,btn)
+        document.querySelector('#book-list').append(li)
+    }
+
+// Event handlers 
+    function handleForm(e){
+        e.preventDefault()
+        //Builds Book
+        const book = {
+            title: e.target.title.value,
+            author:e.target.author.value,
+            price: e.target.price.value,
+            imageUrl: e.target.imageUrl.value,
+            inventory:e.target.inventory.value,
+            reviews:[]
+        }
+        renderBookCard(book)
+    }
+
+
+//Invoking functions
+    renderHeader(bookStore)
+    renderFooter(bookStore)
+    bookStore.inventory.forEach(renderBookCard)
+    document.querySelector('#book-form').addEventListener('submit', handleForm)
+
+
+})
