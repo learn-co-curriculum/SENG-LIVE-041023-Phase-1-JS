7d497d9c5775491cc1f37ac0df4313148c7efc91
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-04-17 11:27:12 -0400

    patch

--- 06_PATCH_and_DELETE_Requests/src/index.js
+++ 06_PATCH_and_DELETE_Requests/src/index.js
@@ -1,11 +1,22 @@
+//CRUD   // HTTP verb
+
+//CREATE //POST
+
+//READ   //GET
+
+//UPDATE  //PATCH/PUT
+
+//DELETE //DELETE
+
 document.addEventListener('DOMContentLoaded', () => {
     // Fetch requests 
-        // Function for making a GET request 
+        // GET request 
         function fetchResource(url){
             return fetch(url)
             .then(res => res.json())
         }
 
+        // POST request
         function createResources(url, body){
             return fetch(url,{
                 method: 'POST', 
@@ -15,6 +26,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 body: JSON.stringify(body),
             })
             .then(res => res.json())
+        }
+
+        //PATCH request
+        function updateResources(url, body){
+            return fetch(url,{
+                method: 'PATCH', 
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body), //JS obj -> JSON obj
+            })
+            .then(res => res.json()) //JSON obj => JS obj
         }
 
     // Rendering functions
@@ -35,21 +58,31 @@ document.addEventListener('DOMContentLoaded', () => {
             const h3 = document.createElement('h3')
             const pAuthor = document.createElement('p')
             const pPrice = document.createElement('p')
+            const pInventory = document.createElement('input')
             const img = document.createElement('img')
             const btn = document.createElement('button')
     
             h3.textContent = cardData.title
             pAuthor.textContent = cardData.author
+            pInventory.type = 'number'
+            pInventory.value = cardData.inventory
             pPrice.textContent = `$${cardData.price}`
             btn.textContent = 'Delete'
-    
+
             img.src = cardData.imageUrl
             li.className = 'list-li'
+
+           
     
             //Event Listeners 
-            btn.addEventListener('click',()=>li.remove())
+                //delete
+                btn.addEventListener('click',()=>li.remove())
+
+                //patch 
+                pInventory.addEventListener("change", (e) =>  handlePatch(e, cardData))
+
         
-            li.append(h3,pAuthor,pPrice,img,btn)
+            li.append(h3,pAuthor,pPrice,pInventory, img,btn)
             document.querySelector('#book-list').append(li)
         }
     
@@ -83,6 +116,16 @@ document.addEventListener('DOMContentLoaded', () => {
         fetchResource('http://localhost:3000/books')
         .then(books => books.forEach(renderBookCard))
         .catch(e => console.error(e))
+
+        //PATCH
+        function handlePatch(e, cardData){
+            e.preventDefault()
+            updateResources(`http://localhost:3000/books/${cardData.id}`
+            , {inventory: e.target.value}) //
+            .then(data => console.log(data))
+            .catch(console.error)
+        }
+       
     
         document.querySelector('#book-form').addEventListener('submit', handleForm)
     
