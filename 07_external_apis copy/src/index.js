fba031b7c01033e916fdaa85094fa6971b5f3241
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-04-21 09:54:50 -0400

    07

--- 07_external_apis copy/src/index.js
+++ 07_external_apis copy/src/index.js
@@ -1,9 +1,9 @@
 console.log("hello")
 console.log(apiKEY)
-// fetch('https://pokeapi.co/api/v2/pokemon/mewtwo/')
+// fetch('https://pokeapi.co/api/v2/pokemon/ditto')
 // .then(res => res.json())
 // .then(data =>  {
-//     debugger
+//     //debugger
 //     console.log(data.name)
 // })
 
@@ -82,58 +82,105 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         function handleRenderSearch(){
-            console.log("helloo")
             document.querySelector('main').innerHTML = `
             <form id="api-Search">
                 <label>API Search<label>
-                <input type="text" name="search"></input>
+                <input type="text" id="search" name="search"></input>
                 <input type="submit"></input>
             </form>
             `
-
             document.querySelector('#api-search').addEventListener('submit', handleAPIQuery)
         }
+
         //Handles Google Books API search
         function handleAPIQuery(e){
             e.preventDefault()
-            
-            const search = e.target.search.value
+            console.log(e.target.search.value)
+            const search = document.querySelector('#search').value
             console.log(search)
+           //const search = e.target.search.value
 
+            
             fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=20&key=${apiKEY}`)
-            .then(res => res.json())
-            .then(apiBooks => {
- 
-            
-                apiBooks.items.forEach(book => {
-                
-                    //book.volumeInfo.title
-                    //book.volumeInfo.author -> is an array
-                    //book.volumeInfo.description
-                    //console.log(book)
-                    const div = document.createElement('div')
-                    const h3 = document.createElement('h3')
-                    const h4 = document.createElement('h4')
-                    const p = document.createElement('p')
-                    
-                    h3.textContent = book.volumeInfo.title
-                    
-                    if(book.volumeInfo.authors) h4.textContent = book.volumeInfo.authors.join(' and ')
-                    p.textContent = book.volumeInfo.description
+                .then(response => response.json())
+                .then(data => data.items.map(item=> {
+                    renderResultCard(item.volumeInfo)
+                }))
+                .catch(console.error)
+        }
+        
 
-                    div.className = "search-list"
-               
-                    div.append(h3,h4,p)
-                   
-                    document.querySelector('main').append(div)
+        function renderResultCard(itemInfo){
+            console.log("focused!", itemInfo.authors)
 
-                })
-            })
-       
-            
+            // article as a parent container
+            const bookCard = document.createElement("article")
+
+
+            // h2, two p elements, img
+            const bookTitle = document.createElement("h2")
+            const bookAuthor = document.createElement("p")
+            const bookDescription = document.createElement("p")
+            const bookImage = document.createElement("img")
+
+
+            // add content using textContent
+            bookTitle.textContent = itemInfo.title
+            // if(itemInfo.authors) bookAuthor.textContent = itemInfo.authors.join(' and ')
+            itemInfo.authors? bookAuthor.textContent = itemInfo.authors.join(' and ') : null
+
+            bookDescription.textContent = itemInfo.description
+            itemInfo.imageLinks? bookImage.src = itemInfo.imageLinks.smallThumbnail : null
+
+            bookCard.className = "search-list"
+
+            // wrap elements together into `article` element before appending it to <main>
+            bookCard.append(bookTitle, bookAuthor, bookDescription, bookImage)
+
+            document.querySelector('main').append(bookCard)
+
         }
 
-    
+
+
+
+
+        // function handleAPIQuery(e){
+        //     e.preventDefault()
+            
+        //     const search = e.target.search.value
+        //     console.log(search)
+
+        //     fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=20&key=${apiKEY}`)
+        //     .then(res => res.json())
+        //     .then(apiBooks => {
+ 
+            
+        //         apiBooks.items.forEach(book => {
+                
+        //             //book.volumeInfo.title
+        //             //book.volumeInfo.author -> is an array
+        //             //book.volumeInfo.description
+        //             //console.log(book)
+        //             const div = document.createElement('div')
+        //             const h3 = document.createElement('h3')
+        //             const h4 = document.createElement('h4')
+        //             const p = document.createElement('p')
+                    
+        //             h3.textContent = book.volumeInfo.title
+                    
+        //             if(book.volumeInfo.authors) h4.textContent = book.volumeInfo.authors.join(' and ')
+        //             p.textContent = book.volumeInfo.description
+
+        //             div.className = "search-list"
+               
+        //             div.append(h3,h4,p)
+                   
+        //             document.querySelector('main').append(div)
+
+        //         })
+        //     })
+        // }   
     
     // Invoking functions    
         fetchResource('http://localhost:3000/stores/1')
@@ -144,8 +191,8 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(e => console.error(e))
     
         fetchResource('http://localhost:3000/books')
-        .then(books => books.forEach(renderBookCard))
-        .catch(e => console.error(e))
+            .then(books => books.forEach(renderBookCard))
+            .catch(e => console.error(e))
     
         document.querySelector('#book-form').addEventListener('submit', handleForm)
         document.querySelector('#nav-search').addEventListener('click', handleRenderSearch)
